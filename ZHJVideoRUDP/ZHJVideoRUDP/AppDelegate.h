bc2c6fe793d6e4ae36c56c755fbc4e4322a5c34d
zhj <zhj@zhjdeiMac.local>
2018-10-09 11:11:45 +0800

    初始化项目

---
+++ ZHJVideoRUDP/ZHJVideoRUDP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHJVideoRUDP
+//
+//  Created by zhj on 2018/10/9.
+//  Copyright © 2018年 zhj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZHJVideoRUDP/ZHJVideoRUDP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZHJVideoRUDP
+//
+//  Created by zhj on 2018/10/9.
+//  Copyright © 2018年 zhj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
